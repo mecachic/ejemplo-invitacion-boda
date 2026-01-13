eefcb1b5c9eb2cb9a46011b7c28d0fe25309fabf
mecachic <sara.ah.contact@gmail.com>
2026-01-13 23:31:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === "github" ? "/ejemplo-invitacion-boda/" : "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+    },
+  },
 }));
